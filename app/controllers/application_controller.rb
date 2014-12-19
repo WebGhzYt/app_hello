27ccea482b65cb67d6d6f2dc2130695fb0c3454f
Rohit <engrohitjain5@gmail.com>
2014-12-18 18:26:25 -0800

    heroku setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-		render text: "This is our first page when client ping the url of site"
-	end
+  		render text: "hello, world!Project is rohit_app"
+  end
 
 end
-	
-
